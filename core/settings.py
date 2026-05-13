c4abfad237913c75ba5cca8af5968e11218ff244
Lucca Rocha <oliveirarochalucca@outlook.com>
2026-05-12 23:50:58 -0300

    Fix authentication configuration for Swagger

--- core/settings.py
+++ core/settings.py
@@ -179,22 +179,4 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'API para gerenciar jogos',
     'VERSION': '1.0.0',
     'SERVE_PERMISSIONS': ['rest_framework.permissions.AllowAny'],
-    'AUTHENTICATION_WHITELIST': [
-        'jogos.authentication.FlexibleTokenAuthentication',
-    ],
-    'SECURITY': [
-        {
-            'tokenAuth': []
-        }
-    ],
-    'COMPONENTS': {
-        'securitySchemes': {
-            'tokenAuth': {
-                'type': 'apiKey',
-                'in': 'header',
-                'name': 'Authorization',
-                'description': 'Token-based authentication with `Token` prefix. Example: `Token 9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b`'
-            }
-        }
-    }
 }
